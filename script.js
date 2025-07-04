053ddd01a806e23454a70f317cd4c75e5cf49e40
Rizbin Rahi <rizbin00rahi@gmail.com>
2025-07-04 11:46:15 +0600

    Initial commit: added HTML, CSS, JS, images, and backend script

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById("btnCtg").addEventListener("click",function(){ document.querySelector(".popupContentCatg").style.display= "flex";
+})
+document.getElementById("close-btn").addEventListener("click",function(){ document.querySelector(".popupContentCatg").style.display= "none";
+})
+
+
+document.getElementById("btnOrd").addEventListener("click",function(){ document.querySelector(".popupContentOrd").style.display= "flex";
+})
+document.getElementById("close-btn-Ord").addEventListener("click",function(){ document.querySelector(".popupContentOrd").style.display= "none";
+})
+
+
+document.getElementById("btnPrd").addEventListener("click",function(){ document.querySelector(".popupContentPrd").style.display= "flex";
+})
+document.getElementById("close-btn-Prd").addEventListener("click",function(){ document.querySelector(".popupContentPrd").style.display= "none";
+})
+
+
+document.getElementById("btnHst").addEventListener("click",function(){ document.querySelector(".popupContentHst").style.display= "flex";
+})
+
+
+document.getElementById("btnCrt").addEventListener("click",function(){ document.querySelector(".popupContentCrt").style.display= "flex";
+})
+document.getElementById("close-btn-crt2").addEventListener("click",function(){ document.querySelector(".popupContentCrt").style.display= "none";
+})
+
+
+function onClick(change) {
+  const totalElement = document.getElementById('totalClick');
+  let current = parseInt(totalElement.innerText);
+  
+  let newTotal = current + change;
+  if (newTotal < 0) newTotal = 0;
+
+  totalElement.innerText = newTotal;
+}
